711200f4dd736349693f865ee4e595d32708775c
Igor Lopes <claudioigorai@gmail.com>
2022-09-27 07:53:57 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /* int a = 5;
+        int b = 10;
+        System.out.println("Hello word! " + (a+b));*/
+    }
+}
